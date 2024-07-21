99c0b0fe2d99aa25bd4bf08d28c922cfd49342be
Dieudonne <dieudonnek077@gmail.com>
2024-07-21 12:45:32 +0200

    Fetching Data from an API and Displaying It

--- fetch-data.js
+++ fetch-data.js
@@ -1,28 +1,26 @@
-const fetchUserData = async() => {
-const apiUrl  = 'https://jsonplaceholder.typicode.com/users'
-const dataContainer = document.getElementById('api-data');
-
-try {
-  const response = await fetch(apiUrl);
-  const users = await response.json();
-  dataContainer.innerHTML = '';
-  const userList = document.createElement('ul');
-
-  users.forEach(user => {
-  const userLi = document.createElement('li');
-  userLi.textContent = user.name
-  userList.append(userLi);
-    
-  });
-
-  dataContainer.append(userList)
-
-} catch (error) {
-  dataContainer.innerHTML = '';
-  dataContainer.innerHTML = 'Failed to load user data.';
+async function fetchUserData() {
+  const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+  
+  const dataContainer = document.getElementById('api-data');
+  
+  try {
+      const response = await fetch(apiUrl);
+      const users = await response.json();
+      
+      dataContainer.innerHTML = '';
+      
+      const userList = document.createElement('ul');
+      
+      users.forEach(user => {
+          const listItem = document.createElement('li');
+          listItem.textContent = user.name;
+          userList.appendChild(listItem);
+      });
+      
+      dataContainer.appendChild(userList);
+      
+  } catch (error) {
+      dataContainer.innerHTML = 'Failed to load user data.';
+  }
 }
-}
-
-document.addEventListener('DOMContentLoaded',(Event)=>{
-  fetchUserData();
-})
+document.addEventListener('DOMContentLoaded', fetchUserData);
